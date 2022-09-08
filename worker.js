c4b4f1dd9174bd3ede8c34a2a6a5cd71bf937286
Gao <gaoxiong1212@163.com>
2022-09-09 00:57:54 +0800

    '修改'

--- worker.js
+++ worker.js
@@ -1,3 +1,7 @@
-self.addEventListener('message', function (e) {
-    self.postMessage('You said: ' + e.data);
-  }, false);
+// self.addEventListener('message', function (e) {
+//     self.postMessage('You said: ' + e.data);
+//   }, false);
+
+  onmessage = function(e) {
+    postMessage('俺收到了'+e.data);
+  }
