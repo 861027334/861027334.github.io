f3fcacd92c998a2419672af08c5faf7b4ee5ac99
Gao <861027334@qq.com>
2023-03-11 07:45:17 +0800

    '测试效果'

---
+++ ts/assets/Apply-2ae3782c.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./index-6fb51287.js";const n={};function r(t,l){return c(),o("div",null," hello apply ")}const s=e(n,[["render",r]]);export{s as default};
---
+++ ts/assets/Exception-1190e3a8.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as o}from"./index-6fb51287.js";const n={};function t(r,s){return c(),o("div",null," hello exce ")}const a=e(n,[["render",t]]);export{a as default};
